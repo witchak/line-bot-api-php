667e85d54746c144045a5e8e4355464b37ea232d
witchak <52811411+witchak@users.noreply.github.com>
2019-07-12 15:09:25 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = 'IGP1cCGoyG8ki9iUSVs2zFOMjBrgMzXa2ba278mL7jyeDku4U9zCcElEcil+p9q13Crzu6dKzm2f3qvVOQ499g19bNuGVFXmwBTjSqCkFYt/b134CwWlmqtlx2VLmlk4lYWtXRZ8/xWj4oWg9R5I5wdB04t89/1O/w1cDnyilFU=';
+$access_token = 'woH/MO654EoGAE5scxArC5gbvfa1DZ1CZanq0m+9iJFRaADPaNwH2h8HJmQ986itV2h+Mimbi9ruSN5BWKhWoljTBmViHrMy/J9IZhCAF1h3qoaJlbqPtlXbWdaSH1NHlVEK3LH8OFs7hwkkjuvLXgdB04t89/1O/w1cDnyilFU=';
 
 $url = 'https://api.line.me/v1/oauth/verify';
 
