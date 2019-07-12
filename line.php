91ecbacc7659a992ac6a5c5ea600e78c4c91a59a
witchak <52811411+witchak@users.noreply.github.com>
2019-07-12 14:57:10 +0700

    Update line.php

--- line.php
+++ line.php
@@ -1,7 +1,7 @@
 <?php
 
 $API_URL = 'https://api.line.me/v2/bot/message/reply';
-$ACCESS_TOKEN = 'IG+YXYoYXHnnyNt5Ugfp7c7ME85Cndf63UaJkZICfoa6bDZpEooYURSFFCU7PV+ALGK1yAUxiW+vFrHZJFNIyEmHsWLrx60Y6XMf4R7pr6+mfhGdbxO8xlpxGjPz0uGKb+J4akxmJQCIL0GikF5i6AdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
+$ACCESS_TOKEN = 'woH/MO654EoGAE5scxArC5gbvfa1DZ1CZanq0m+9iJFRaADPaNwH2h8HJmQ986itV2h+Mimbi9ruSN5BWKhWoljTBmViHrMy/J9IZhCAF1h3qoaJlbqPtlXbWdaSH1NHlVEK3LH8OFs7hwkkjuvLXgdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
 $POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
 
 $request = file_get_contents('php://input');   // Get request content
